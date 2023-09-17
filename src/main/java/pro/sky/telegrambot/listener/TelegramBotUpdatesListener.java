4b5b7c9aac744288deee69003c4d90d6a0237be4
Axekill <andrei3.ru@ya.ru>
2023-09-17 20:08:09 +0300

    добавил функцию старт

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,8 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,7 @@ import java.util.List;
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private static final String START = "/start";
 
     @Autowired
     private TelegramBot telegramBot;
@@ -28,9 +31,46 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
+            var text = update.message().text();
+            Long chatId = update.message().chat().id();
+            switch (text) {
+                case START -> {
+                    String userName = update.message().chat().username();
+                    String firstName = update.message().chat().firstName();
+                    String lastName = update.message().chat().lastName();
+                    startCommand(chatId, userName, firstName, lastName);
+                }
+                default -> unknownCommand(chatId);
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
+
+    private void startCommand(Long chatId, String userName, String firstName, String lastName) {
+        String textChat;
+        String fullName = userName + firstName + lastName;
+        if (userName == null & lastName == null) {
+            textChat = "Добро пожаловать в бот, %s !";
+            var formattedText = String.format(textChat, firstName);
+            sendMessage(chatId, formattedText);
+        } else if (fullName == null) {
+            textChat = "Добро пожаловать в бот";
+            sendMessage(chatId, textChat);
+        } else {
+            textChat = "Добро пожаловать в бот, %s !";
+            var formattedText = String.format(textChat, userName);
+            sendMessage(chatId, formattedText);
+        }
+    }
+
+    private void unknownCommand(Long chatId) {
+        var text = "Не удалось распознать команду!";
+        sendMessage(chatId, text);
+    }
+
+    private void sendMessage(Long chatId, String text) {
+        SendMessage message = new SendMessage(chatId, text);
+        SendResponse response = telegramBot.execute(message);
+    }
 }
